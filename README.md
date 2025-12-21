123c736721031b73913bebaca43a548be581ea68
Rainy <bewer.avn@gmail.com>
2025-12-21 02:27:21 +0100

    Lists, Thematic Break, Strikethrough and Underline

--- README.md
+++ README.md
@@ -15,10 +15,31 @@ Italics works with one asterisks, bold two:
 ~~~~
 *Italics*
 **Bold**
+~~Strikethrough~~
+__Underline__
 ~~~~
+## Lists
+Two types: Ordered and unordered. You can nest them however you want, but you should avoid misuing the syntax.
+~~~~
+* List item 1
+* List item 2
+** List item 2.a
+** List item 2.b
+*** List item 2.b.1
+* List item 3
+
+- Ordered list item 1
+- Ordered list item 2
+~~~~
+Again, you can mix and match, just don't be stupid.
 ## Special characters
 Because of the syntax, you must do special things for some characters. Here's a simple table:
 ~~~~
 * : \*
 \ : \\ 		(yes, you need 2 backslashes)
+~~~~
+## Giant linebreak
+Yep, those exist. One is automatically used after Heading 1. To add more, simply
+~~~~
+----
 ~~~~
--- parser.js
+++ parser.js
@@ -1,36 +1,81 @@
+// Takes in a text and returns it in an html format
 export function parser(text) {
-	let html = text;
+	const lines = text.split("\n");
+	let html = "";
+	const listType = [];
 
-	// SPECIAL CHARACTERS
+	for (let line of lines) {
+		line = line.replace(/&/g, "&amp;");						// for & (needs to be first lol)
 
-	html = html.replace(/&/g, "&amp;");							// for & (needs to be first lol)
+		line = line.replace(/\</g, "&lt;");						// for <
+		line = line.replace(/\>/g, "&gt;");						// for >
+		line = line.replace(/\"/g, "&quot;");					// for ""
+		line = line.replace(/\'/g, "&apos;");					// for ''
 
-	html = html.replace(/\</g, "&lt;");							// for <
-	html = html.replace(/\>/g, "&gt;");							// for >
-	html = html.replace(/\"/g, "&quot;");						// for ""
-	html = html.replace(/\'/g, "&apos;");						// for ''
+		line = line.replace(/^# (.*$)/, "<h1>$1</h1><hr>");		// # Heading 1
+		line = line.replace(/^#{2} (.*$)/, "<h2>$1</h2>");		// ## Heading 2
+		line = line.replace(/^#{3} (.*$)/, "<h3>$1</h3>");		// ### Heading 3
+		line = line.replace(/^#{4} (.*$)/, "<h4>$1</h4>");		// #### Heading 4
+		line = line.replace(/^#{5} (.*$)/, "<h5>$1</h5>");		// ##### Heading 5
 
-	// BLOCK MARKUPS
+		line = line.replace(/^----$/gim, "<hr>");				// Line
 
-	html = html.replace(/^# (.*$)/gim, "<h1>$1</h1><hr>");		// # Heading 1
-	html = html.replace(/^#{2} (.*$)/gim, "<h2>$1</h2>");		// ## Heading 2
-	html = html.replace(/^#{3} (.*$)/gim, "<h3>$1</h3>");		// ### Heading 3
-	html = html.replace(/^#{4} (.*$)/gim, "<h4>$1</h4>");		// #### Heading 4
-	html = html.replace(/^#{5} (.*$)/gim, "<h5>$1</h5>");		// ##### Heading 5
+		line = listHandler(line, listType);						// Lists
 
-	html = html.replace(/^(?!<).+/gim, "<p>$&</p>");			// paragraph
+		line = line.replace(/^(?!<).+/gim, "<p>$&</p>");			// paragraph
 
-	// INLINE MARKUPS
+		// INLINE MARKUPS
 
-	html = html.replace(/(?<!\\)\*\*(.*?)\*\*/gim, "<b>$1</b>");// **bold**
-	html = html.replace(/(?<!\\)\*(.*?)\*/gim, "<i>$1</i>");	// *italics*
+		line = line.replace(/(?<!\\)\*\*(.*?)\*\*/gim, "<b>$1</b>");// **bold**
+		line = line.replace(/(?<!\\)\*(.*?)\*/gim, "<i>$1</i>");	// *italics*
+		line = line.replace(/(?<!\\)~~(.*?)~~/gim, "<s>$1</s>");	// ~~strikethrough~~
+		line = line.replace(/(?<!\\)__(.*?)__/gim, "<u>$1</u>");	// __underline__
 
-	// BACKSLASHES (need to be at end)
+		// BACKSLASHES (need to be at end)
 
-	html = html.replace(/\\\\/gim, "&#92;");					// Backslashes 
-	html = html.replace(/\\/gim, "");							// Backslashes
+		line = line.replace(/\\\\/gim, "&#92;");					// Backslashes 
+		line = line.replace(/\\/gim, "");							// Backslashes
 
-	
+		html += line;
+	}
 
-	return html.trim();
+	return html;
+}
+
+function listHandler(line, listType) {
+	const listDictionary = {
+		"*": "ul",
+		"-": "ol"
+	}
+	const prefixCount = countPrefix(line);
+	const listDepth = listType.length;
+
+	let markup = "p";
+	let output = line;
+
+	if (prefixCount > 0) {
+		output = line.substring(prefixCount+1);
+		output = `<li>${output}</li>`;
+
+		for (let i = listDepth; i < prefixCount; i++) {
+			markup = listDictionary[line.charAt(i)];
+			output = `<${markup}>${output}`;
+			listType.push(line.charAt(prefixCount-1));
+		}
+	} 
+
+	for (let i = listDepth; i > prefixCount; i--) {
+		markup = listDictionary[listType.pop()];
+		output = `</${markup}>\n${output}`;
+	}
+
+	return output;
+}
+
+// Counts how many time a "*" and "-" are before a line, without a space
+function countPrefix(line) {
+	const regex = new RegExp(`^[*\-]+(?= )`);
+	const prefixCount = line.match(regex)?.[0].length ?? 0;
+
+	return prefixCount;
 }
